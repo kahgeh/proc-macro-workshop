7f445ef6e81963d46db0d15e381668717b6694c6
kahgeh <kahgeh@hotmail.com>
2021-12-30 23:11:02 +1100

    support Option typed fields, don't throw error when it's optional, also for ergonomics use the option inner type as param input type

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,6 +2,26 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Data::Struct, DataStruct, DeriveInput, Fields, FieldsNamed};
 
+fn get_option_inner_type(ty: &syn::Type) -> Option<&syn::Type> {
+    if let syn::Type::Path(ref p) = ty {
+        if p.path.segments.len() != 1 || p.path.segments.iter().last().unwrap().ident != "Option" {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref inner_type) = p.path.segments[0].arguments {
+            if inner_type.args.len() != 1 {
+                return None;
+            }
+
+            let inner_type = inner_type.args.first().unwrap();
+            if let syn::GenericArgument::Type(ref t) = inner_type {
+                return Some(t);
+            }
+        }
+    }
+    None
+}
+
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     eprintln!("tokens: {}", input);
@@ -26,26 +46,44 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let optionised_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        quote! { #name: std::option::Option<#ty> }
+        if get_option_inner_type(ty).is_some() {
+            quote! { #name: #ty }
+        } else {
+            quote! { #name: std::option::Option<#ty> }
+        }
     });
 
     let author_methods = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-
-        quote! {
-            fn #name(&mut self, #name: #ty)->&mut Self {
-                self.#name = Some(#name);
-                self
+        if let Some(inner_ty) = get_option_inner_type(ty) {
+            quote! {
+                fn #name(&mut self, #name: #inner_ty)->&mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            }
+        } else {
+            quote! {
+                fn #name(&mut self, #name: #ty)->&mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }
     });
 
     let build_fields = fields.iter().map(|f| {
         let name = &f.ident;
-
-        quote! {
-            #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+        let ty = &f.ty;
+        if get_option_inner_type(ty).is_some() {
+            quote! {
+                #name: self.#name.clone()
+            }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or(concat!(stringify!(#name), " is not set"))?
+            }
         }
     });
 
