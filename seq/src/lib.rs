f69d7907f0592ce437e0fa7ef3255bcaacee3171
kahgeh <kahgeh@hotmail.com>
2022-01-10 12:32:40 +1100

    parse header

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,26 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse::Parse, parse_macro_input, Token};
+
+#[derive(Debug)]
+struct SequenceMacroInput {}
+
+impl Parse for SequenceMacroInput {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        input.parse::<syn::Ident>()?;
+        input.parse::<Token![in]>()?;
+        input.parse::<syn::Lit>()?;
+        input.parse::<Token![..]>()?;
+        input.parse::<syn::Lit>()?;
+        input.parse::<syn::ExprBlock>()?;
+        Ok(SequenceMacroInput {})
+    }
+}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let ast = parse_macro_input!(input as SequenceMacroInput);
+    eprintln!("{:#?}", ast);
+    let expanded = quote! { /****/};
+    expanded.into()
 }
