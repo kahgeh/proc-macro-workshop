13f5b0dfd08d87e0bae37fb42bce73703e913482
kahgeh <kahgeh@hotmail.com>
2022-01-10 21:43:51 +1100

    parsed ast includes body

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -3,19 +3,23 @@ use quote::quote;
 use syn::{parse::Parse, parse_macro_input, Token};
 
 #[derive(Debug)]
-struct SequenceMacroInput {}
+struct SequenceMacroInput {
+    range: syn::ExprRange,
+    body: proc_macro2::TokenStream,
+}
 
 impl Parse for SequenceMacroInput {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         input.parse::<syn::Ident>()?;
         input.parse::<Token![in]>()?;
-        input.parse::<syn::Lit>()?;
-        input.parse::<Token![..]>()?;
-        input.parse::<syn::Lit>()?;
-        input.parse::<syn::ExprBlock>()?;
-        Ok(SequenceMacroInput {})
+        Ok(SequenceMacroInput {
+            range: input.parse::<syn::ExprRange>()?,
+            body: proc_macro2::TokenStream::parse(input)?,
+        })
     }
 }
+
+// impl From<SequenceMacroInput> for TokenStream {}
 
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
